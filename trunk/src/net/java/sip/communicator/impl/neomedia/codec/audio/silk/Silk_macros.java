9d17b65a4c0ee3c9f19b979080fea36787b88cce
xdxn2010 <xdxn2010@2f42bd5d-cc32-194c-7e6e-455a7b4de88b>
2011-05-09 16:04:26 +0000

    Modification of Silk_macros.java and Silk_range_coder.java, including license, comments, coding style, translation check and the like.

--- trunk/src/net/java/sip/communicator/impl/neomedia/codec/audio/silk/Silk_macros.java
+++ trunk/src/net/java/sip/communicator/impl/neomedia/codec/audio/silk/Silk_macros.java
@@ -1,99 +1,86 @@
-/**
- * Translated from the C code of Skype SILK codec (ver. 1.0.6)
- * Downloaded from http://developer.skype.com/silk/
- * 
- * Class "Silk_macros" is mainly based on 
- * ../SILK_SDK_SRC_FLP_v1.0.6/src/SKP_Silk_macros.h
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
  */
 package net.java.sip.communicator.impl.neomedia.codec.audio.silk;
 
 /**
- *
+ * Translated from what is an inline header file for general platform.
+ * 
  * @author Jing Dai
+ * @author Dingxin Xu
  */
 public class Silk_macros 
 {
 	// (a32 * (SKP_int32)((SKP_int16)(b32))) >> 16 output have to be 32bit int
-//	#define SKP_SMULWB(a32, b32)            ((((a32) >> 16) * (SKP_int32)((SKP_int16)(b32))) + ((((a32) & 0x0000FFFF) * (SKP_int32)((SKP_int16)(b32))) >> 16))
 	static int SKP_SMULWB(int a32, int b32)
 	{
 		return ((((a32) >> 16) * (int)((short)(b32))) + ((((a32) & 0x0000FFFF) * (int)((short)(b32))) >> 16));
 	}
 
 	// a32 + (b32 * (SKP_int32)((SKP_int16)(c32))) >> 16 output have to be 32bit int
-//	#define SKP_SMLAWB(a32, b32, c32)       ((a32) + ((((b32) >> 16) * (SKP_int32)((SKP_int16)(c32))) + ((((b32) & 0x0000FFFF) * (SKP_int32)((SKP_int16)(c32))) >> 16)))
 	static int SKP_SMLAWB(int a32, int b32, int c32)
 	{
 		return ((a32) + ((((b32) >> 16) * (int)((short)(c32))) + ((((b32) & 0x0000FFFF) * (int)((short)(c32))) >> 16)));
 	}
 
 	// (a32 * (b32 >> 16)) >> 16
-//	#define SKP_SMULWT(a32, b32)            (((a32) >> 16) * ((b32) >> 16) + ((((a32) & 0x0000FFFF) * ((b32) >> 16)) >> 16))
 	static int SKP_SMULWT(int a32, int b32)
 	{
 		return (((a32) >> 16) * ((b32) >> 16) + ((((a32) & 0x0000FFFF) * ((b32) >> 16)) >> 16));
 	}
 
 	// a32 + (b32 * (c32 >> 16)) >> 16
-//	#define SKP_SMLAWT(a32, b32, c32)       ((a32) + (((b32) >> 16) * ((c32) >> 16)) + ((((b32) & 0x0000FFFF) * ((c32) >> 16)) >> 16))
 	static int SKP_SMLAWT(int a32, int b32, int c32)
 	{
 		return ((a32) + (((b32) >> 16) * ((c32) >> 16)) + ((((b32) & 0x0000FFFF) * ((c32) >> 16)) >> 16));
 	}
 
 	// (SKP_int32)((SKP_int16)(a3))) * (SKP_int32)((SKP_int16)(b32)) output have to be 32bit int
-//	#define SKP_SMULBB(a32, b32)            ((SKP_int32)((SKP_int16)(a32)) * (SKP_int32)((SKP_int16)(b32)))
 	static int SKP_SMULBB(int a32, int b32)
 	{
 		return ((int)((short)(a32)) * (int)((short)(b32)));
 	}
 
 	// a32 + (SKP_int32)((SKP_int16)(b32)) * (SKP_int32)((SKP_int16)(c32)) output have to be 32bit int
-//	#define SKP_SMLABB(a32, b32, c32)       ((a32) + ((SKP_int32)((SKP_int16)(b32))) * (SKP_int32)((SKP_int16)(c32)))
 	static int SKP_SMLABB(int a32, int b32, int c32)
 	{
 		return ((a32) + ((int)((short)(b32))) * (int)((short)(c32)));
 	}
 
 	// (SKP_int32)((SKP_int16)(a32)) * (b32 >> 16)
-//	#define SKP_SMULBT(a32, b32)            ((SKP_int32)((SKP_int16)(a32)) * ((b32) >> 16))
 	static int SKP_SMULBT(int a32, int b32)
 	{
 		return ((int)((short)(a32)) * ((b32) >> 16));
 	}
 
 	// a32 + (SKP_int32)((SKP_int16)(b32)) * (c32 >> 16)
-//	#define SKP_SMLABT(a32, b32, c32)       ((a32) + ((SKP_int32)((SKP_int16)(b32))) * ((c32) >> 16))
 	static int SKP_SMLABT(int a32, int b32, int c32)
 	{
 		return ((a32) + ((int)((short)(b32))) * ((c32) >> 16));
 	}
 
 	// a64 + (b32 * c32)
-//	#define SKP_SMLAL(a64, b32, c32)        (SKP_ADD64((a64), ((SKP_int64)(b32) * (SKP_int64)(c32))))
 	static long SKP_SMLAL(long a64, int b32, int c32)
 	{
 		return a64 + (long)b32 * (long)c32;
 	}
 
 	// (a32 * b32) >> 16
-//	#define SKP_SMULWW(a32, b32)            SKP_MLA(SKP_SMULWB((a32), (b32)), (a32), SKP_RSHIFT_ROUND((b32), 16))
 	static int SKP_SMULWW(int a32, int b32)
 	{
 		return SKP_SMULWB(a32, b32) + a32 * Silk_SigProc_FIX.SKP_RSHIFT_ROUND(b32, 16);
 	}
 
 	// a32 + ((b32 * c32) >> 16)
-//	#define SKP_SMLAWW(a32, b32, c32)       SKP_MLA(SKP_SMLAWB((a32), (b32), (c32)), (b32), SKP_RSHIFT_ROUND((c32), 16))
 	static int SKP_SMLAWW(int a32, int b32, int c32)
 	{
 		return SKP_SMLAWB(a32, b32, c32) + b32 * Silk_SigProc_FIX.SKP_RSHIFT_ROUND(c32, 16);
 	}
 
 	/* add/subtract with output saturated */
-//	#define SKP_ADD_SAT32(a, b)             ((((a) + (b)) & 0x80000000) == 0 ?                              \
-//	                                        ((((a) & (b)) & 0x80000000) != 0 ? SKP_int32_MIN : (a)+(b)) :   \
-//	                                        ((((a) | (b)) & 0x80000000) == 0 ? SKP_int32_MAX : (a)+(b)) )
 	static int SKP_ADD_SAT32(int a, int b)
 	{
 		if( ((a + b) & 0x80000000) == 0 )
@@ -102,9 +89,6 @@ public class Silk_macros
 			return ((a | b) & 0x80000000) == 0 ? Integer.MAX_VALUE : a+b;
 	}
 
-//	#define SKP_SUB_SAT32(a, b)             ((((a)-(b)) & 0x80000000) == 0 ?                                        \
-//	                                        (( (a) & ((b)^0x80000000) & 0x80000000) ? SKP_int32_MIN : (a)-(b)) :    \
-//	                                        ((((a)^0x80000000) & (b)  & 0x80000000) ? SKP_int32_MAX : (a)-(b)) )
 	static int SKP_SUB_SAT32(int a, int b)
 	{
 		if( ((a - b) & 0x80000000) == 0 )
@@ -115,65 +99,11 @@ public class Silk_macros
 	    
 	static int SKP_Silk_CLZ16(short in16)
 	{
-//	    int out32 = 0;
-//	    if( in16 == 0 )
-//	    {
-//	        return 16;
-//	    }
-//	    /* test nibbles */
-//	    if( (in16 & 0xFF00) != 0 )
-//	    {
-//	        if( (in16 & 0xF000) != 0 ) 
-//	        {
-//	            in16 >>= 12;
-//	        }
-//	        else 
-//	        {
-//	            out32 += 4;
-//	            in16 >>= 8;
-//	        }
-//	    } 
-//	    else 
-//	    {
-//	        if( (in16 & 0xFFF0) != 0 ) 
-//	        {
-//	            out32 += 8;
-//	            in16 >>= 4;
-//	        }
-//	        else 
-//	        {
-//	            out32 += 12;
-//	        }
-//	    }
-//	    /* test bits and return */
-//	    if( (in16 & 0xC) != 0 )
-//	    {
-//	        if( (in16 & 0x8) != 0 )
-//	            return out32 + 0;
-//	        else
-//	            return out32 + 1;
-//	    }
-//	    else 
-//	    {
-//	        if( (in16 & 0xE) !=0 )
-//	            return out32 + 2;
-//	        else
-//	            return out32 + 3;
-//	    }
 		return Integer.numberOfLeadingZeros((int)in16 & 0x0000FFFF) - 16;
 	}
 
 	static int SKP_Silk_CLZ32(int in32)
 	{
-//	    /* test highest 16 bits and convert to SKP_int16 */
-//	    if( (in32 & 0xFFFF0000) !=0 ) 
-//	    {
-//	        return SKP_Silk_CLZ16((short)(in32 >> 16));
-//	    }
-//	    else
-//	    {
-//	        return SKP_Silk_CLZ16((short)in32) + 16;
-//	    }
 		return Integer.numberOfLeadingZeros(in32);
 	}
 }
--- trunk/src/net/java/sip/communicator/impl/neomedia/codec/audio/silk/Silk_range_coder.java
+++ trunk/src/net/java/sip/communicator/impl/neomedia/codec/audio/silk/Silk_range_coder.java
@@ -1,24 +1,26 @@
-/**
- * Translated from the C code of Skype SILK codec (ver. 1.0.6)
- * Downloaded from  http://developer.skype.com/silk/
- * 
- * Class "Silk_range_coder" is mainly based on 
- *../SILK_SDK_SRC_FLP_v1.0.6/src/SKP_Silk_range_coder.c
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
  */
 package net.java.sip.communicator.impl.neomedia.codec.audio.silk;
 
 /**
- *
+ * Range coder
+ * 
  * @author Jing Dai
+ * @author Dingxin Xu
  */
 public class Silk_range_coder 
 {
-	/* Range encoder for one symbol */
-//	void SKP_Silk_range_encoder(
-//	    SKP_Silk_range_coder_state      *psRC,              /* I/O  compressor data structure                   */
-//	    const SKP_int                   data,               /* I    uncompressed data                           */
-//	    const SKP_uint16                prob[]              /* I    cumulative density functions                */
-//	)
+    /**
+     * Range encoder for one symbol.
+     * @param psRC compressor data structure.
+     * @param data uncompressed data.
+     * @param prob cumulative density functions.
+     * @param prob_offset offset of valid data.
+     */
 	static void SKP_Silk_range_encoder(
 		    SKP_Silk_range_coder_state      psRC,               /* I/O  compressor data structure                   */
 		    final int                       data,               /* I    uncompressed data                           */
@@ -26,18 +28,13 @@ public class Silk_range_coder
 		    int				        		prob_offset                                     	
 	)
 	{
-//	    SKP_uint32 low_Q16, high_Q16;
-//	    SKP_uint32 base_tmp, range_Q32;
 	    long low_Q16, high_Q16;
 	    long base_tmp, range_Q32;
 
 	    /* Copy structure data */
-//	    SKP_uint32 base_Q32  = psRC.base_Q32;
 	    long base_Q32  = psRC.base_Q32;
-//	    SKP_uint32 range_Q16 = psRC.range_Q16;
 	    long range_Q16 = psRC.range_Q16;
 	    int  bufferIx  = psRC.bufferIx;
-//	    SKP_uint8  *buffer   = psRC.buffer;
 	    byte[] buffer = psRC.buffer;
 
 	    if( psRC.error!=0 )
@@ -49,9 +46,7 @@ public class Silk_range_coder
 	    low_Q16  = prob[ prob_offset + data ];
 	    high_Q16 = prob[ prob_offset + data + 1 ];
 	    base_tmp = base_Q32; /* save current base, to test for carry */
-//djinn ??	    
-//	    base_Q32 += SKP_MUL_uint( range_Q16, low_Q16 );
-//	    range_Q32 = SKP_MUL_uint( range_Q16, high_Q16 - low_Q16 );
+
 	    base_Q32 += ( range_Q16 * low_Q16 ) & 0xFFFFFFFFL;
 	    range_Q32 = ( range_Q16 * (high_Q16 - low_Q16) )& 0xFFFFFFFFL;
 
@@ -64,20 +59,16 @@ public class Silk_range_coder
 	    }
 
 	    /* Check normalization */
-//	    if( range_Q32 & 0xFF000000 ) {
 	    if( (range_Q32 & 0xFF000000)!=0 ) 
 	    {
 	        /* No normalization */
-//	        range_Q16 = SKP_RSHIFT_uint( range_Q32, 16 );
 	        range_Q16 = ( range_Q32 >> 16 );
 	    }
 	    else
 	    {
-//	        if( range_Q32 & 0xFFFF0000 ) 
 	        if( (range_Q32 & 0xFFFF0000)!=0 ) 
 	        {
 	            /* Normalization of 8 bits shift */
-//	            range_Q16 = SKP_RSHIFT_uint( range_Q32, 8 );
 	            range_Q16 = ( range_Q32 >> 8 );
 	        }
 	        else 
@@ -110,14 +101,14 @@ public class Silk_range_coder
 	    psRC.range_Q16 = range_Q16;
 	    psRC.bufferIx  = bufferIx;
 	}
-
-	/* Range encoder for multiple symbols */
-//	static void SKP_Silk_range_encoder_multi(
-//	    SKP_Silk_range_coder_state      psRC,              /* I/O  compressor data structure                   */
-//	    int[]                   data,             /* I    uncompressed data    [nSymbols]             */
-//	    const SKP_uint16 * const        prob[],             /* I    cumulative density functions                */
-//	    final int                   nSymbols            /* I    number of data symbols                      */
-//	)
+	
+	/**
+	 * Range encoder for multiple symbols.
+	 * @param psRC compressor data structure.
+	 * @param data uncompressed data    [nSymbols].
+	 * @param prob cumulative density functions.
+	 * @param nSymbols number of data symbols.
+	 */
 	static void SKP_Silk_range_encoder_multi(
 		    SKP_Silk_range_coder_state  psRC,              /* I/O  compressor data structure                   */
 		    int[]                   	data,             /* I    uncompressed data    [nSymbols]             */
@@ -128,18 +119,19 @@ public class Silk_range_coder
 	    int k;
 	    for( k = 0; k < nSymbols; k++ ) 
 	    {
-//	        SKP_Silk_range_encoder( psRC, data[ k ], prob[ k ] );
 	        SKP_Silk_range_encoder( psRC, data[ k ], prob[ k ], 0 );
 	    }
 	}
-
-	/* Range decoder for one symbol */
-//	void SKP_Silk_range_decoder(
-//	    SKP_int                         data[],             /* O    uncompressed data                           */
-//	    SKP_Silk_range_coder_state      *psRC,              /* I/O  compressor data structure                   */
-//	    const SKP_uint16                prob[],             /* I    cumulative density function                 */
-//	    SKP_int                         probIx              /* I    initial (middle) entry of cdf               */
-//	)
+	
+	/**
+	 * Range decoder for one symbol.
+	 * @param data uncompressed data.
+	 * @param data_offset offset of valid data.
+	 * @param psRC compressor data structure.
+	 * @param prob cumulative density function.
+	 * @param prob_offset offset of valid data.
+	 * @param probIx initial (middle) entry of cdf.
+	 */
 	static void SKP_Silk_range_decoder(
 		    int                             data[],             /* O    uncompressed data                           */
 		    int								data_offset,
@@ -156,26 +148,21 @@ public class Silk_range_coder
 	    long base_Q32  = psRC.base_Q32;
 	    long range_Q16 = psRC.range_Q16;
 	    int  bufferIx  = psRC.bufferIx;
-//djinn ??	    SKP_uint8  *buffer   = &psRC.buffer[ 4 ];
 	    byte[] buffer  = psRC.buffer;
 	    int    buffer_offset = 4;
 
 	    if( psRC.error != 0) {
 	        /* Set output to zero */
-//	        *data = 0;
 	    	data[data_offset + 0] = 0;
 	        return;
 	    }
 	    
-//    high_Q16 = prob[ probIx ];
 	    high_Q16 = prob[prob_offset + probIx];
 	    
-//djinn ??	    base_tmp = SKP_MUL_uint( range_Q16, high_Q16 );
 	    base_tmp = Silk_SigProc_FIX.SKP_MUL_uint( range_Q16, high_Q16 );
 	    
 	    if( base_tmp > base_Q32 ) {
 	        while( true ) {
-//djinn ???	            low_Q16 = prob[ --probIx  ];
 	            low_Q16 = prob[ --probIx + prob_offset  ];
 	            base_tmp = Silk_SigProc_FIX.SKP_MUL_uint( range_Q16, low_Q16 );
 	            if( base_tmp <= base_Q32 ) {
@@ -186,7 +173,6 @@ public class Silk_range_coder
 	            if( high_Q16 == 0 ) {
 	                psRC.error = Silk_define.RANGE_CODER_CDF_OUT_OF_RANGE;
 	                /* Set output to zero */
-//	                *data = 0;
 	                data[data_offset + 0] = 0;
 	                return;
 	            }
@@ -204,36 +190,30 @@ public class Silk_range_coder
 	            if( high_Q16 == 0xFFFF ) {
 	                psRC.error = Silk_define.RANGE_CODER_CDF_OUT_OF_RANGE;
 	                /* Set output to zero */
-//	                *data = 0;
 	                data[data_offset + 0] = 0;
 	                return;
 	            }
 	        }
 	    }
-//	    *data = probIx;
 	    data[data_offset + 0] = probIx;
 	    
 	    base_Q32 -= Silk_SigProc_FIX.SKP_MUL_uint( range_Q16, low_Q16 );
 	    range_Q32 = Silk_SigProc_FIX.SKP_MUL_uint( range_Q16, high_Q16 - low_Q16 );
 
 	    /* Check normalization */
-	    if( (range_Q32 & 0xFF000000) != 0 ) {
-//or 	    if( (range_Q32 & 0xFF000000L) != 0 ) {
+//TODO:or 	    if( (range_Q32 & 0xFF000000) != 0 ) {
+	    if( (range_Q32 & 0xFF000000L) != 0 ) {
 	        /* No normalization */
-//	        range_Q16 = SKP_RSHIFT_uint( range_Q32, 16 );
 	    	range_Q16 = range_Q32>>>16;
 	    } else {
-	        if( (range_Q32 & 0xFFFF0000) != 0 ) {
-//or	        if( (range_Q32 & 0xFFFF0000L) != 0 ) {
+//TODO:or	        if( (range_Q32 & 0xFFFF0000) != 0 ) {
+	        if( (range_Q32 & 0xFFFF0000L) != 0 ) {
 	            /* Normalization of 8 bits shift */
-//	            range_Q16 = SKP_RSHIFT_uint( range_Q32, 8 );\
 	        	range_Q16 = ( range_Q32 >>> 8 );
 	            /* Check for errors */
-//	            if( SKP_RSHIFT_uint( base_Q32, 24 ) ) {
 	        	if( ( base_Q32 >>> 24 ) != 0) {
 	                psRC.error = Silk_define.RANGE_CODER_NORMALIZATION_FAILED;
 	                /* Set output to zero */
-//	                *data = 0;
 	                data[data_offset + 0] = 0;
 	                return;
 	            }
@@ -241,33 +221,25 @@ public class Silk_range_coder
 	            /* Normalization of 16 bits shift */
 	            range_Q16 = range_Q32;
 	            /* Check for errors */
-//	            if( SKP_RSHIFT( base_Q32, 16 ) ) {
 	            if( ( base_Q32 >> 16 ) != 0 ) {
 	                psRC.error = Silk_define.RANGE_CODER_NORMALIZATION_FAILED;
 	                /* Set output to zero */
-//	                *data = 0;
 	                data[data_offset + 0] = 0;
 	                return;
 	            }
 	            /* Update base */
-//	            base_Q32 = SKP_LSHIFT_uint( base_Q32, 8 );
 	            base_Q32 = ( base_Q32 << 8 );
 	            /* Make sure not to read beyond buffer */
 	            if( bufferIx < psRC.bufferLength ) {
 	                /* Read one byte from buffer */
-//djinn ??
-//	            	base_Q32 |= (long)buffer[buffer_offset + bufferIx++ ];
 	            	base_Q32 |= ((int)buffer[buffer_offset + bufferIx++ ])&0xFF;
 	            }
 	        }
 	        /* Update base */
-//	        base_Q32 = SKP_LSHIFT_uint( base_Q32, 8 );
 	        base_Q32 = ( base_Q32 << 8 );
 	        /* Make sure not to read beyond buffer */
 	        if( bufferIx < psRC.bufferLength ) {
 	            /* Read one byte from buffer */
-//	            base_Q32 |= (long)buffer[ bufferIx++ ];
-	//        	base_Q32 |= (long)buffer[ buffer_offset + bufferIx++ ];
 	        	base_Q32 |= ((int)buffer[ buffer_offset + bufferIx++ ])&0xFF;
 	        }
 	    }
@@ -276,7 +248,6 @@ public class Silk_range_coder
 	    if( range_Q16 == 0 ) {
 	        psRC.error = Silk_define.RANGE_CODER_ZERO_INTERVAL_WIDTH;
 	        /* Set output to zero */
-//            *data = 0;
             data[data_offset + 0] = 0;
 	        return;
 	    }
@@ -286,15 +257,15 @@ public class Silk_range_coder
 	    psRC.range_Q16 = range_Q16;
 	    psRC.bufferIx  = bufferIx;
 	}
-
-	/* Range decoder for multiple symbols */
-//	void SKP_Silk_range_decoder_multi(
-//	    SKP_int                         data[],             /* O    uncompressed data                [nSymbols] */
-//	    SKP_Silk_range_coder_state      *psRC,              /* I/O  compressor data structure                   */
-//	    const SKP_uint16 * const        prob[],             /* I    cumulative density functions                */
-//	    const SKP_int                   probStartIx[],      /* I    initial (middle) entries of cdfs [nSymbols] */
-//	    const SKP_int                   nSymbols            /* I    number of data symbols                      */
-//	)
+	
+	/**
+	 * Range decoder for multiple symbols.
+	 * @param data uncompressed data [nSymbols].
+	 * @param psRC compressor data structure.
+	 * @param prob cumulative density functions.
+	 * @param probStartIx initial (middle) entries of cdfs [nSymbols].
+	 * @param nSymbols number of data symbols.
+	 */
 	static void SKP_Silk_range_decoder_multi(
 		    int                         data[],             /* O    uncompressed data                [nSymbols] */
 		    SKP_Silk_range_coder_state  psRC,              /* I/O  compressor data structure                   */
@@ -305,15 +276,14 @@ public class Silk_range_coder
 	{
 	    int  k;
 	    for( k = 0; k < nSymbols; k++ ) {
-//	        SKP_Silk_range_decoder( &data[ k ], psRC, prob[ k ], probStartIx[ k ] );
 	    	SKP_Silk_range_decoder( data, k, psRC, prob[ k ], 0, probStartIx[ k ] );
 	    }
 	}
 
-	/* Initialize range encoder */
-//	void SKP_Silk_range_enc_init(
-//	    SKP_Silk_range_coder_state      *psRC               /* O    compressor data structure                   */
-//	)
+	/**
+	 * Initialize range encoder.
+	 * @param psRC compressor data structure.
+	 */
 	static void SKP_Silk_range_enc_init(
 		    SKP_Silk_range_coder_state      psRC               /* O    compressor data structure                   */
 	)
@@ -325,13 +295,14 @@ public class Silk_range_coder
 	    psRC.base_Q32     = 0;
 	    psRC.error        = 0;
 	}
-
-	/* Initialize range decoder */
-//	void SKP_Silk_range_dec_init(
-//	    SKP_Silk_range_coder_state      *psRC,              /* O    compressor data structure                   */
-//	    const SKP_uint8                 buffer[],           /* I    buffer for compressed data [bufferLength]   */
-//	    const int                 bufferLength        /* I    buffer length (in bytes)                    */
-//	)
+	
+	/**
+	 * Initialize range decoder.
+	 * @param psRC compressor data structure.
+	 * @param buffer buffer for compressed data [bufferLength].
+	 * @param buffer_offset offset of valid data.
+	 * @param bufferLength buffer length (in bytes).
+	 */
 	static void SKP_Silk_range_dec_init(
 		    SKP_Silk_range_coder_state  psRC,              /* O    compressor data structure                   */
 		    byte                        buffer[],           /* I    buffer for compressed data [bufferLength]   */
@@ -346,17 +317,10 @@ public class Silk_range_coder
 	    }
 	    /* Initialize structure */
 	    /* Copy to internal buffer */
-//	    SKP_memcpy( psRC.buffer, buffer, bufferLength * sizeof( SKP_uint8 ) ); 
 	    System.arraycopy(buffer, buffer_offset, psRC.buffer, 0, bufferLength);
 	    
 	    psRC.bufferLength = bufferLength;
-//djinn ??? consider  buffer_offset with bufferIx ???
 	    psRC.bufferIx = 0;
-//	    psRC.base_Q32 = 
-//	        SKP_LSHIFT_uint( (long)buffer[ 0 ], 24 ) | 
-//	        SKP_LSHIFT_uint( (long)buffer[ 1 ], 16 ) | 
-//	        SKP_LSHIFT_uint( (long)buffer[ 2 ],  8 ) | 
-//	                         (long)buffer[ 3 ];
 	    psRC.base_Q32 = 
 	    	(
 		        ( ((int)buffer[ buffer_offset + 0 ] & 0xFF) << 24 ) | 
@@ -367,12 +331,13 @@ public class Silk_range_coder
 	    psRC.range_Q16 = 0x0000FFFF;
 	    psRC.error     = 0;
 	}
-
-	/* Determine length of bitstream */
-//	SKP_int SKP_Silk_range_coder_get_length(                /* O    returns number of BITS in stream            */
-//	    const SKP_Silk_range_coder_state    *psRC,          /* I    compressed data structure                   */
-//	    SKP_int                             *nBytes         /* O    number of BYTES in stream                   */
-//	)
+	
+	/**
+	 * Determine length of bitstream.
+	 * @param psRC compressed data structure.
+	 * @param nBytes number of BYTES in stream.
+	 * @return returns number of BITS in stream.
+	 */
 	static int SKP_Silk_range_coder_get_length(                /* O    returns number of BITS in stream            */
 		    SKP_Silk_range_coder_state    psRC,          /* I    compressed data structure                   */
 		    int[]                         nBytes         /* O    number of BYTES in stream                   */
@@ -381,16 +346,17 @@ public class Silk_range_coder
 	    int nBits;
 
 	    /* Number of bits in stream */
-//	    nBits = SKP_LSHIFT( psRC.bufferIx, 3 ) + SKP_Silk_CLZ32( psRC.range_Q16 - 1 ) - 14;
 	    nBits = ( psRC.bufferIx << 3 ) + Silk_macros.SKP_Silk_CLZ32((int) (psRC.range_Q16 - 1) ) - 14;
 	    
-//	    *nBytes = SKP_RSHIFT( nBits + 7, 3 );
 	    nBytes [0] = (( nBits + 7)>> 3 );
 	    /* Return number of bits in bitstream */
 	    return nBits;
 	}
-
-	/* Write shortest uniquely decodable stream to buffer, and determine its length */
+	
+	/**
+	 * Write shortest uniquely decodable stream to buffer, and determine its length.
+	 * @param psRC ompressed data structure.
+	 */
 	static void SKP_Silk_range_enc_wrap_up(
 	    SKP_Silk_range_coder_state      psRC               /* I/O  compressed data structure                   */
 	)
@@ -401,7 +367,6 @@ public class Silk_range_coder
 	    /* Lower limit of interval, shifted 8 bits to the right */
 	    base_Q24 = psRC.base_Q32 >>> 8;
 
-//	    bits_in_stream = SKP_Silk_range_coder_get_length( psRC, &nBytes );
 	    int[] nBytes_ptr = new int[1];
 	    bits_in_stream = SKP_Silk_range_coder_get_length( psRC, nBytes_ptr );
 	    nBytes = nBytes_ptr[0];
@@ -423,13 +388,11 @@ public class Silk_range_coder
 	    /* Store to stream, making sure not to write beyond buffer */
 	    if( psRC.bufferIx < psRC.bufferLength ) 
 	    {
-//	        psRC.buffer[ psRC.bufferIx++ ] = (SKP_uint8)SKP_RSHIFT_uint( base_Q24, 16 );
 	        psRC.buffer[ psRC.bufferIx++ ] = (byte) ( base_Q24>>> 16 );
 	        if( bits_to_store > 8 ) 
 	        {
 	            if( psRC.bufferIx < psRC.bufferLength ) 
 	            {
-//	                psRC.buffer[ psRC.bufferIx++ ] = (SKP_uint8)SKP_RSHIFT_uint( base_Q24, 8 );
 	                psRC.buffer[ psRC.bufferIx++ ] = (byte) ( base_Q24 >>> 8 );
 	            }
 	        }
@@ -445,18 +408,17 @@ public class Silk_range_coder
 	        }
 	    }
 	}
-
-	/* Check that any remaining bits in the last byte are set to 1 */
-//	void SKP_Silk_range_coder_check_after_decoding(
-//	    SKP_Silk_range_coder_state      *psRC               /* I/O  compressed data structure                   */
-//	)
+	
+	/**
+	 * Check that any remaining bits in the last byte are set to 1.
+	 * @param psRC compressed data structure.
+	 */
 	static void SKP_Silk_range_coder_check_after_decoding(
 		    SKP_Silk_range_coder_state      psRC               /* I/O  compressed data structure                   */
 	)
 	{
 	    int bits_in_stream, nBytes, mask;
 	    int nBytes_ptr[] = new int[1];
-//	    nBytes_ptr[0] = nBytes;
 	    
 	    bits_in_stream = SKP_Silk_range_coder_get_length( psRC, nBytes_ptr );
 	    nBytes = nBytes_ptr[0];
@@ -469,7 +431,6 @@ public class Silk_range_coder
 
 	    /* Test any remaining bits in last byte */
 	    if( (bits_in_stream & 7) != 0 ) {
-//	        mask = SKP_RSHIFT( 0xFF, bits_in_stream & 7 );
 	    	mask = ( 0xFF >>( bits_in_stream & 7) );
 	    	
 	        if( ( psRC.buffer[ nBytes - 1 ] & mask ) != mask ) {
@@ -478,5 +439,4 @@ public class Silk_range_coder
 	        }
 	    }
 	}
-
 }
